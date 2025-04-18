c6924ffbf02884de017c611d5d97cfc42249b219
Lrain <49682559+dc514lrain@users.noreply.github.com>
2025-04-18 08:24:53 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    # store settings config
+
+    def __init__(self):
+        # screen config
+        self.screen_width = 1200
+        self.screen_hight = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 10
